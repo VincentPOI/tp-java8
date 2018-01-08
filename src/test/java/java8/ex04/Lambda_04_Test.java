e2c41acd1e2b7414a7af6c25a8b9c33510d371fa
vincent <vincentpoisson08gmail.com>
2018-01-08 17:21:50 +0100

    lambda 04 réindentation

--- src/test/java/java8/ex04/Lambda_04_Test.java
+++ src/test/java/java8/ex04/Lambda_04_Test.java
@@ -1,6 +1,5 @@
 package java8.ex04;
 
-
 import java8.data.Account;
 import java8.data.Data;
 import java8.data.Person;
@@ -11,114 +10,104 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Exercice 04 - FuncCollection
- * Exercice synthèse des exercices précédents
+ * Exercice 04 - FuncCollection Exercice synthèse des exercices précédents
  */
 public class Lambda_04_Test {
 
-    // tag::interfaces[]
-    interface GenericPredicate<T> {
-        boolean test(T t);
-    }
+	// tag::interfaces[]
+	interface GenericPredicate<T> {
+		boolean test(T t);
+	}
 
-    interface GenericMapper<T, E> {
-        E map(T t);
-    }
+	interface GenericMapper<T, E> {
+		E map(T t);
+	}
 
-    interface Processor<T> {
-        void process(T t);
-    }
-    // end::interfaces[]
+	interface Processor<T> {
+		void process(T t);
+	}
+	// end::interfaces[]
 
-    // tag::FuncCollection[]
-    class FuncCollection<T> {
+	// tag::FuncCollection[]
+	class FuncCollection<T> {
 
-        private Collection<T> list = new ArrayList<>();
+		private Collection<T> list = new ArrayList<>();
 
-        public void add(T a) {
-            list.add(a);
-        }
-
-        public void addAll(Collection<T> all) {
-            for(T el:all) {
-                list.add(el);
-            }
-        }
-    // end::FuncCollection[]
-
-        // tag::methods[]
-        private FuncCollection<T> filter(GenericPredicate<T> predicate) {
-            FuncCollection<T> result = new FuncCollection<>(); 
-            for (T collec : list){
-            	if (predicate.test(collec)) {
-    				result.add(collec);
-    			};
-            }
-            return result;
-        }
-
-        private <E> FuncCollection<E> map(GenericMapper<T, E> mapper) {
-            FuncCollection<E> result = new FuncCollection<>();
-            for (T collec : list) {			
-				result.add(mapper.map(collec));
+		public void add(T a) {
+			list.add(a);
 		}
-            return result;
-        }
 
-        private void forEach(Processor<T> processor) {
-        	for(T t : list){
-         	   processor.process(t);
-            }
-        }
-        // end::methods[]
+		public void addAll(Collection<T> all) {
+			for (T el : all) {
+				list.add(el);
+			}
+		}
+		// end::FuncCollection[]
 
-    }
+		// tag::methods[]
+		private FuncCollection<T> filter(GenericPredicate<T> predicate) {
+			FuncCollection<T> result = new FuncCollection<>();
+			for (T collec : list) {
+				if (predicate.test(collec)) {
+					result.add(collec);
+				}
+				;
+			}
+			return result;
+		}
 
+		private <E> FuncCollection<E> map(GenericMapper<T, E> mapper) {
+			FuncCollection<E> result = new FuncCollection<>();
+			for (T collec : list) {
+				result.add(mapper.map(collec));
+			}
+			return result;
+		}
 
+		private void forEach(Processor<T> processor) {
+			for (T t : list) {
+				processor.process(t);
+			}
+		}
+		// end::methods[]
 
-    // tag::test_filter_map_forEach[]
-    @Test
-    public void test_filter_map_forEach() throws Exception {
+	}
 
-        List<Person> personList = Data.buildPersonList(100);
-        FuncCollection<Person> personFuncCollection = new FuncCollection<>();
-        personFuncCollection.addAll(personList);
-        personFuncCollection
-                // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50       		
-                .filter( p -> p.getAge() > 50)
-                // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
-                .map(p -> new Account(p, 1000))
-                // TODO vérifier que chaque compte a un solde à 1000.
-                // TODO vérifier que chaque titulaire de compte a un age > 50
-                .forEach(a -> {
-                	assert a.getBalance() == 1000;
-                	assert a.getOwner().getAge() > 50;
-                });
-    }
-    // end::test_filter_map_forEach[]
+	// tag::test_filter_map_forEach[]
+	@Test
+	public void test_filter_map_forEach() throws Exception {
 
-    // tag::test_filter_map_forEach_with_vars[]
-    @Test
-    public void test_filter_map_forEach_with_vars() throws Exception {
+		List<Person> personList = Data.buildPersonList(100);
+		FuncCollection<Person> personFuncCollection = new FuncCollection<>();
+		personFuncCollection.addAll(personList);
+		personFuncCollection
+				.filter(p -> p.getAge() > 50)
+				.map(p -> new Account(p, 1000))
+				.forEach(a -> {
+					assert a.getBalance() == 1000;
+					assert a.getOwner().getAge() > 50;
+				});
+	}
+	// end::test_filter_map_forEach[]
 
-        List<Person> personList = Data.buildPersonList(100);
-        FuncCollection<Person> personFuncCollection = new FuncCollection<>();
-        personFuncCollection.addAll(personList);
+	// tag::test_filter_map_forEach_with_vars[]
+	@Test
+	public void test_filter_map_forEach_with_vars() throws Exception {
 
-        GenericPredicate<Person> filterByAge = p -> p.getAge() > 50;
-        GenericMapper<Person,Account> mapToAccount = p -> new Account(p, 1000);
-        Processor<Account> verifyAccount = a -> {
-        	assert a.getBalance() == 1000;
-        	assert a.getOwner().getAge() > 50;
-        };
+		List<Person> personList = Data.buildPersonList(100);
+		FuncCollection<Person> personFuncCollection = new FuncCollection<>();
+		personFuncCollection.addAll(personList);
 
-        personFuncCollection
-                .filter(filterByAge)
-                .map(mapToAccount)
-                .forEach(verifyAccount);
+		GenericPredicate<Person> filterByAge = p -> p.getAge() > 50;
+		GenericMapper<Person, Account> mapToAccount = p -> new Account(p, 1000);
+		Processor<Account> verifyAccount = a -> {
+			assert a.getBalance() == 1000;
+			assert a.getOwner().getAge() > 50;
+		};
 
-    }
-    // end::test_filter_map_forEach_with_vars[]
+		personFuncCollection.filter(filterByAge).map(mapToAccount).forEach(verifyAccount);
 
+	}
+	// end::test_filter_map_forEach_with_vars[]
 
 }
